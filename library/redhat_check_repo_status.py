ecd7750d96496baea605cb8d66e09a83d337a677
arijitroy003 <arijitroy003@gmail.com>
2026-07-23 17:15:40 +0530

    Fix UnboundLocalError when repo file does not exist
    Initialize repostat to an empty dict in the else branch so it is
    defined when accessed later.  Also guard failmsg access behind the
    exists check and use fail_json for error paths instead of exit_json.

--- library/redhat_check_repo_status.py
+++ library/redhat_check_repo_status.py
@@ -117,8 +117,8 @@ def main():
       rc,repostat = redhat_check_repo_status( repos, status, repofile )
     else:
        rc = 1
+       repostat = {}
        failmsg="Errors occurred! File [%s] does not exist!" % repofile
-      
 
     tempjson={}
     if rc == 0:
@@ -128,8 +128,9 @@ def main():
     else:
        repostat['rc'] = rc
        tempjson['repostat']=repostat
-       tempjson['failmsg']=failmsg
-       module.exit_json(rc=rc, **tempjson)
+       if not exists:
+          tempjson['failmsg']=failmsg
+       module.fail_json(msg="Repository check failed", **tempjson)
  
 # import module snippets
 from ansible.module_utils.basic import *
